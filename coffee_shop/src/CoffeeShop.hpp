917cbdb6dcdeae01a2d3bbfe7d113c6ca77a693f
Kate <fukuro.s@yandex.ru>
2025-12-02 19:34:51 +0300

    added coffee shop

---
+++ coffee_shop/src/CoffeeShop.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <deque>
+#include <mutex>
+#include <condition_variable>
+#include <memory>
+#include <thread>
+#include <vector>
+#include "Order.hpp"
+
+class CoffeeShop {
+public:
+    CoffeeShop();
+    ~CoffeeShop();
+
+    void start(size_t num_baristas);
+    void stop();
+
+    void submitOrder(std::shared_ptr<Order> order);
+    bool cancelOrder(int orderId);
+
+private:
+    void baristaLoop(int idx);
+
+    std::deque<std::shared_ptr<Order>> queue_;
+    std::mutex queue_mtx_;
+    std::condition_variable cv_;
+    bool shutdown_ = false;
+    std::vector<std::thread> baristas_;
+};
